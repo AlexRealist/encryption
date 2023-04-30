a06c75e9fc18c6eb1e5cd6c64295de8d2ebb600d
Alex <alex.respect555@gmail.com>
2023-04-30 16:14:02 +0300

    шифрование

---
+++ encryption.rb
@@ -0,0 +1,23 @@
+require 'digest' # подсоединил метод шифрования
+
+puts "Введите слово или фразу для шифрования:"
+
+user = gets.chomp
+puts "Каким способом зашифровать:"
+puts "1. MD5"
+puts "2. SHA1"
+
+user_way = gets.chomp.to_i
+
+until user_way.between?(1, 2)
+  puts "Введите 1 или 2"
+  user_way = gets.chomp.to_i
+end
+
+puts "Результат"
+
+if user_way == 1
+  puts Digest::MD5.hexdigest(user)
+else
+  puts Digest::SHA1.hexdigest(user)
+end
